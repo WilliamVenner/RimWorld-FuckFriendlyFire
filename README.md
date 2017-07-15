1c5c8160eba7912de1fc90d86320a9172195b235
William Venner <william@venner.io>
2017-07-15 02:07:51 +0100

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# RimWorld-NoFriendlyFire
-[A17] Disables Friendly Fire in RimWorld
+## NoFriendlyFire
+
+(This mod has no requirements)
+
+Don't you just hate it when a colonist shoots another accidentally?
+This mod stops bullets from hitting:
+
+* Your colonists
+* Your animals
+
+It doesn't stop bullets from hitting:
+
+* Enemies
+* Friendly factions
+* Anything you specifically target
---
+++ Source/NoFriendlyFire.cs
@@ -0,0 +1,39 @@
+﻿using Verse;
+using Harmony;
+using RimWorld;
+using System;
+using System.Reflection;
+
+namespace NoFriendlyFire
+{
+
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            var harmony = HarmonyInstance.Create("venner.io.nofriendlyfire");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(Bullet), "Impact", new Type[] { typeof(Thing) })]
+    public partial class NoFriendlyFireBullet_Impact
+    {
+        static void Prefix(Bullet __instance, ref Thing hitThing)
+        {
+            try
+            {
+
+                Type t = __instance.GetType().BaseType;
+
+                Thing launcher       = (Thing)t.GetField("launcher",       BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+                Thing assignedTarget = (Thing)t.GetField("assignedTarget", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+
+                if (hitThing.Faction.Equals(launcher.Faction) && assignedTarget != hitThing)
+                    hitThing = null;
+
+            } catch (NullReferenceException) { }
+        }
+    }
+}
